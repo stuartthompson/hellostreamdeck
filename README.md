5d2727c45978ee247b6c2ea2baf20eeb19a1a2fa
Stuart Thompson <stuart@stuartthompson.net>
2020-05-01 10:43:37 -0700

    Parsing StreamDeck startup params.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Hello StreamDeck
 
 Learning how to build plugins for the Elgato StreamDeck.
+
+## Test Run
+
+cargo run -- -port 1234 -pluginUUID test.com -registerEvent evt -info inf
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,29 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let mut port: String = String::from("");
+    let mut plugin_uuid: String = String::from("");
+    let mut register_event: String = String::from("");
+    let mut info: String = String::from("");
+
+    for ix in 0..args.len() {
+        if args[ix] == "-port" {
+            port = args[ix+1].clone();
+        }
+        if args[ix] == "-pluginUUID" {
+            plugin_uuid = args[ix+1].clone();
+        }
+        if args[ix] == "-registerEvent" {
+            register_event = args[ix+1].clone();
+        }
+        if args[ix] == "-info" {
+            info = args[ix+1].clone();
+        }
+    }
+
+    println!("Port: {0}", port);
+    println!("PluginUUID: {0}", plugin_uuid);
+    println!("RegisterEvent: {0}", register_event);
+    println!("Info: {0}", info);
 }
